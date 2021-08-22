a87a445ec65af9f76461bab7388c441b52ffa196
Rajib Chakravorty <4748396+rajibchakravorty@users.noreply.github.com>
2021-08-22 17:00:31 +1000

    hamiltonian construction layer updated

---
+++ qdata/system_layers/hamiltonian_construction_layer.py
@@ -0,0 +1,97 @@
+"""
+This class defines a custom tensorflow layer that takes the Hamiltonian parameters
+as input, and generates the Hamiltonain matrix as an output at each time step for
+each example in the batch
+"""
+
+import numpy as np
+from tensorflow import (
+    add_n,
+    cast,
+    complex64,
+    concat,
+    constant,
+    expand_dims,
+    multiply,
+    shape,
+    tile
+    )
+from tensorflow.keras import layers
+
+
+class HamiltonianConstruction(layers.Layer):
+    """Class constructor
+
+    dynamic_operators: a list of all operators that have time-varying coefficients
+    static_operators : a list of all operators that have constant coefficients
+    """
+
+    def __init__(self, dynamic_operators, static_operators, **kwargs):
+        self.dynamic_operators = [constant(op, dtype=complex64) for op in dynamic_operators]
+        self.static_operators = [constant(op, dtype=complex64) for op in static_operators]
+        self.dim = dynamic_operators[0].shape[-1]
+
+        # this has to be called for any tensorflow custom layer
+        super().__init__(**kwargs)
+
+    def call(self, inputs):     # pylint: disable=arguments-differ
+        """
+        This method must be defined for any custom layer, it is where the calculations are done.
+
+        inputs: a tensor representing the inputs to the layer. This is passed automatically
+        by tensorflow.
+        """
+
+        hamiltonians = []
+        # loop over the strengths of all dynamic operators
+
+        for idx_op, dynamic_op in enumerate(self.dynamic_operators):
+            # select the particular strength of the operator
+            hamiltonian = cast(inputs[:, :, :, idx_op:idx_op + 1], complex64)
+
+            # construct a tensor in the form of a row vector whose elements are [d1,d2,d3, 1,1],
+            # where d1, d2, and d3 correspond to the number of examples,
+            # number of time steps of the input, and number of realizations
+            temp_shape = concat(
+                [shape(inputs)[0:3], constant(np.array([1, 1], dtype=np.int32))], 0)
+
+            # add two extra dimensions for batch, time, and realization
+            operator = expand_dims(dynamic_op, 0)
+            operator = expand_dims(operator, 0)
+            operator = expand_dims(operator, 0)
+
+            # repeat the pauli operators along the batch and time dimensions
+            operator = tile(operator, temp_shape)
+
+            # repeat the pulse waveform to as dxd matrix
+            temp_shape = constant(np.array([1, 1, 1, self.dim, self.dim], dtype=np.int32))
+            hamiltonian = expand_dims(hamiltonian, -1)
+            hamiltonian = tile(hamiltonian, temp_shape)
+
+            # Now multiply each operator with its corresponding strength element-wise and
+            # add to the list of Hamiltonians
+            hamiltonians.append(multiply(operator, hamiltonian))
+
+        # loop over the strengths of all static operators
+        for static_op in self.static_operators:
+            # construct a tensor in the form of a row vector whose elements are [d1,d2,d3,1,1],
+            # where d1, d2, and d2 correspond to the number of examples, number of time steps
+            # of the input, and number of realizations
+            temp_shape = concat(
+                [shape(inputs)[0:3], constant(np.array([1, 1], dtype=np.int32))], 0)
+
+            # add two extra dimensions for batch and time
+            operator = expand_dims(static_op, 0)
+            operator = expand_dims(operator, 0)
+            operator = expand_dims(operator, 0)
+
+            # repeat the pauli operators along the batch and time dimensions
+            operator = tile(operator, temp_shape)
+
+            # Now add to the list of Hamiltonians
+            hamiltonians.append(operator)
+
+        # now add all components together
+        hamiltonians = add_n(hamiltonians)
+
+        return hamiltonians
--- qdata/utilities/check_noise.py
+++ qdata/utilities/check_noise.py
@@ -2,7 +2,7 @@
 This function calculates the coherence measurements to check the noise behaviour, based on the simulation parameters passed as a dictionary
 """
 
-from numpy import average, eye
+from numpy import average, eye, zeros
 from numpy.linalg import norm
 
 from ..system_layers import QuantumTFSimulator
@@ -18,7 +18,7 @@ def check_noise(sim_parameters):
         sim_parameters["num_pulses"], False, sim_parameters["noise_profile"])
     # 3) Run the simulator and collect the results
     print("Running the simulation\n")
-    simulation_results = simulator.simulate(np.zeros((1,)), batch_size=1)
+    simulation_results = simulator.simulate(zeros((1,)), batch_size=1)
     H0, H1, U0, Uc, UI, expectations = simulation_results[4:10]
     Vo = simulation_results[10:]
     Vo = [average(V, axis=1) for V in Vo]
